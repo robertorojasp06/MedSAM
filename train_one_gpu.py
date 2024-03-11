6f52011cbee77ddfbeef6bd5b27f9325a2a92280
Roberto Rojas <roberto.rojasp06@gmail.com>
2024-03-11 14:51:33 -0300

    Update script to train MedSAM with 1 GPU.
    - Refactoring.
    - Add normalization of slice image arrays to 0-1 range.

--- train_one_gpu.py
+++ train_one_gpu.py
@@ -11,7 +11,6 @@ import os
 
 join = os.path.join
 from tqdm import tqdm
-from skimage import transform
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
@@ -82,126 +81,39 @@ class NpyDataset(Dataset):
         )  # (1024, 1024, 3)
         # convert the shape to (3, H, W)
         img_1024 = np.transpose(img_1024, (2, 0, 1))
+        if np.max(img_1024) > 1:
+            img_1024 = img_1024 / 255
         assert (
             np.max(img_1024) <= 1.0 and np.min(img_1024) >= 0.0
         ), "image should be normalized to [0, 1]"
         gt = np.load(
-            self.gt_path_files[index], "r", allow_pickle=True
+            self.gt_path_files[index], "r+", allow_pickle=True
         )  # multiple labels [0, 1,4,5...], (256,256)
         assert img_name == os.path.basename(self.gt_path_files[index]), (
             "img gt name error" + self.gt_path_files[index] + self.npy_files[index]
         )
         label_ids = np.unique(gt)[1:]
-        gt2D = np.uint8(
-            gt == random.choice(label_ids.tolist())
-        )  # only one label, (256, 256)
-        assert np.max(gt2D) == 1 and np.min(gt2D) == 0.0, "ground truth should be 0, 1"
-        y_indices, x_indices = np.where(gt2D > 0)
-        x_min, x_max = np.min(x_indices), np.max(x_indices)
-        y_min, y_max = np.min(y_indices), np.max(y_indices)
-        # add perturbation to bounding box coordinates
-        H, W = gt2D.shape
-        x_min = max(0, x_min - random.randint(0, self.bbox_shift))
-        x_max = min(W, x_max + random.randint(0, self.bbox_shift))
-        y_min = max(0, y_min - random.randint(0, self.bbox_shift))
-        y_max = min(H, y_max + random.randint(0, self.bbox_shift))
-        bboxes = np.array([x_min, y_min, x_max, y_max])
-        return (
-            torch.tensor(img_1024).float(),
-            torch.tensor(gt2D[None, :, :]).long(),
-            torch.tensor(bboxes).float(),
-            img_name,
-        )
-
-
-# %% sanity test of dataset class
-tr_dataset = NpyDataset("data/npy/CT_Abd")
-tr_dataloader = DataLoader(tr_dataset, batch_size=8, shuffle=True)
-for step, (image, gt, bboxes, names_temp) in enumerate(tr_dataloader):
-    print(image.shape, gt.shape, bboxes.shape)
-    # show the example
-    _, axs = plt.subplots(1, 2, figsize=(25, 25))
-    idx = random.randint(0, 7)
-    axs[0].imshow(image[idx].cpu().permute(1, 2, 0).numpy())
-    show_mask(gt[idx].cpu().numpy(), axs[0])
-    show_box(bboxes[idx].numpy(), axs[0])
-    axs[0].axis("off")
-    # set title
-    axs[0].set_title(names_temp[idx])
-    idx = random.randint(0, 7)
-    axs[1].imshow(image[idx].cpu().permute(1, 2, 0).numpy())
-    show_mask(gt[idx].cpu().numpy(), axs[1])
-    show_box(bboxes[idx].numpy(), axs[1])
-    axs[1].axis("off")
-    # set title
-    axs[1].set_title(names_temp[idx])
-    # plt.show()
-    plt.subplots_adjust(wspace=0.01, hspace=0)
-    plt.savefig("./data_sanitycheck.png", bbox_inches="tight", dpi=300)
-    plt.close()
-    break
-
-# %% set up parser
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-i",
-    "--tr_npy_path",
-    type=str,
-    default="data/npy/CT_Abd",
-    help="path to training npy files; two subfolders: gts and imgs",
-)
-parser.add_argument("-task_name", type=str, default="MedSAM-ViT-B")
-parser.add_argument("-model_type", type=str, default="vit_b")
-parser.add_argument(
-    "-checkpoint", type=str, default="work_dir/SAM/sam_vit_b_01ec64.pth"
-)
-# parser.add_argument('-device', type=str, default='cuda:0')
-parser.add_argument(
-    "--load_pretrain", type=bool, default=True, help="use wandb to monitor training"
-)
-parser.add_argument("-pretrain_model_path", type=str, default="")
-parser.add_argument("-work_dir", type=str, default="./work_dir")
-# train
-parser.add_argument("-num_epochs", type=int, default=1000)
-parser.add_argument("-batch_size", type=int, default=2)
-parser.add_argument("-num_workers", type=int, default=0)
-# Optimizer parameters
-parser.add_argument(
-    "-weight_decay", type=float, default=0.01, help="weight decay (default: 0.01)"
-)
-parser.add_argument(
-    "-lr", type=float, default=0.0001, metavar="LR", help="learning rate (absolute lr)"
-)
-parser.add_argument(
-    "-use_wandb", type=bool, default=False, help="use wandb to monitor training"
-)
-parser.add_argument("-use_amp", action="store_true", default=False, help="use amp")
-parser.add_argument(
-    "--resume", type=str, default="", help="Resuming training from checkpoint"
-)
-parser.add_argument("--device", type=str, default="cuda:0")
-args = parser.parse_args()
-
-if args.use_wandb:
-    import wandb
-
-    wandb.login()
-    wandb.init(
-        project=args.task_name,
-        config={
-            "lr": args.lr,
-            "batch_size": args.batch_size,
-            "data_path": args.tr_npy_path,
-            "model_type": args.model_type,
-        },
-    )
-
-# %% set up model for training
-# device = args.device
-run_id = datetime.now().strftime("%Y%m%d-%H%M")
-model_save_path = join(args.work_dir, args.task_name + "-" + run_id)
-device = torch.device(args.device)
-# %% set up model
+        if len(label_ids) > 0:
+            gt2D = np.uint8(
+                gt == random.choice(label_ids.tolist())
+            )  # only one label, (256, 256)
+            assert np.max(gt2D) == 1 and np.min(gt2D) == 0.0, "ground truth should be 0, 1"
+            y_indices, x_indices = np.where(gt2D > 0)
+            x_min, x_max = np.min(x_indices), np.max(x_indices)
+            y_min, y_max = np.min(y_indices), np.max(y_indices)
+            # add perturbation to bounding box coordinates
+            H, W = gt2D.shape
+            x_min = max(0, x_min - random.randint(0, self.bbox_shift))
+            x_max = min(W, x_max + random.randint(0, self.bbox_shift))
+            y_min = max(0, y_min - random.randint(0, self.bbox_shift))
+            y_max = min(H, y_max + random.randint(0, self.bbox_shift))
+            bboxes = np.array([x_min, y_min, x_max, y_max])
+            return (
+                torch.tensor(img_1024).float(),
+                torch.tensor(gt2D[None, :, :]).long(),
+                torch.tensor(bboxes).float(),
+                img_name,
+            )
 
 
 class MedSAM(nn.Module):
@@ -249,6 +161,97 @@ class MedSAM(nn.Module):
 
 
 def main():
+    # %% set up parser
+    parser = argparse.ArgumentParser(
+        description="""Train the image encoder and mask decoder (freeze prompt
+        image encoder).""",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-i",
+        "--tr_npy_path",
+        type=str,
+        default="data/npy/CT_Abd",
+        help="path to training npy files; two subfolders: gts and imgs",
+    )
+    parser.add_argument("-task_name", type=str, default="MedSAM-ViT-B")
+    parser.add_argument("-model_type", type=str, default="vit_b")
+    parser.add_argument(
+        "-checkpoint", type=str, default="work_dir/SAM/sam_vit_b_01ec64.pth"
+    )
+    # parser.add_argument('-device', type=str, default='cuda:0')
+    parser.add_argument(
+        "--load_pretrain", type=bool, default=True, help="use wandb to monitor training"
+    )
+    parser.add_argument("-pretrain_model_path", type=str, default="")
+    parser.add_argument("-work_dir", type=str, default="./work_dir")
+    # train
+    parser.add_argument("-num_epochs", type=int, default=1000)
+    parser.add_argument("-batch_size", type=int, default=2)
+    parser.add_argument("-num_workers", type=int, default=0)
+    # Optimizer parameters
+    parser.add_argument(
+        "-weight_decay", type=float, default=0.01, help="weight decay (default: 0.01)"
+    )
+    parser.add_argument(
+        "-lr", type=float, default=0.0001, metavar="LR", help="learning rate (absolute lr)"
+    )
+    parser.add_argument(
+        "-use_wandb", type=bool, default=False, help="use wandb to monitor training"
+    )
+    parser.add_argument("-use_amp", action="store_true", default=False, help="use amp")
+    parser.add_argument(
+        "--resume", type=str, default="", help="Resuming training from checkpoint"
+    )
+    parser.add_argument("--device", type=str, default="cuda:0")
+    args = parser.parse_args()
+
+    if args.use_wandb:
+        import wandb
+
+        wandb.login()
+        wandb.init(
+            project=args.task_name,
+            config={
+                "lr": args.lr,
+                "batch_size": args.batch_size,
+                "data_path": args.tr_npy_path,
+                "model_type": args.model_type,
+            },
+        )
+
+    # %% sanity test of dataset class
+    tr_dataset = NpyDataset(args.tr_npy_path)
+    tr_dataloader = DataLoader(tr_dataset, batch_size=8, shuffle=True)
+    for step, (image, gt, bboxes, names_temp) in enumerate(tr_dataloader):
+        print(image.shape, gt.shape, bboxes.shape)
+        # show the example
+        _, axs = plt.subplots(1, 2, figsize=(25, 25))
+        idx = random.randint(0, 7)
+        axs[0].imshow(image[idx].cpu().permute(1, 2, 0).numpy())
+        show_mask(gt[idx].cpu().numpy(), axs[0])
+        show_box(bboxes[idx].numpy(), axs[0])
+        axs[0].axis("off")
+        # set title
+        axs[0].set_title(names_temp[idx])
+        idx = random.randint(0, 7)
+        axs[1].imshow(image[idx].cpu().permute(1, 2, 0).numpy())
+        show_mask(gt[idx].cpu().numpy(), axs[1])
+        show_box(bboxes[idx].numpy(), axs[1])
+        axs[1].axis("off")
+        # set title
+        axs[1].set_title(names_temp[idx])
+        # plt.show()
+        plt.subplots_adjust(wspace=0.01, hspace=0)
+        plt.savefig("./data_sanitycheck.png", bbox_inches="tight", dpi=300)
+        plt.close()
+        break
+    # %% set up model for training
+    # device = args.device
+    run_id = datetime.now().strftime("%Y%m%d-%H%M")
+    model_save_path = join(args.work_dir, args.task_name + "-" + run_id)
+    device = torch.device(args.device)
+    # %% set up model
     os.makedirs(model_save_path, exist_ok=True)
     shutil.copyfile(
         __file__, join(model_save_path, run_id + "_" + os.path.basename(__file__))
